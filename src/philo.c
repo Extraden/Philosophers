4dab4ca71d959c73991bbe40605391ec3a5fd47d
Extraden <extraden7@gmail.com>
2025-09-08 23:41:39 +0200

    fix: refactor main function to separate monitoring loop for improved readability and maintainability

--- src/philo.c
+++ src/philo.c
@@ -15,44 +15,42 @@
 #include "philo.h"
 #include <stdlib.h>
 
+static int monitoring_loop(t_data *data)
+{
+  while (1)
+  {
+	usleep(250);
+	pthread_mutex_lock((&data->stop_mutex));
+	if (data->stop == 1)
+	{
+		pthread_mutex_unlock((&data->stop_mutex));
+		end_simulation(data);
+		return (0);
+	}
+	pthread_mutex_unlock((&data->stop_mutex));
+  }
+}
+
 int	main(int argc, char *argv[])
 {
 	t_data	data;
 
-	if ((argc != 5) && (argc != 6))
-	{
-		printf("Wrong number of arguments\n");
-		printf("Usage:\n");
-		printf("./philo number_of_philosophers time_to_die time_to_eat time_to_sleep [maximum_meals]\n");
-		return (0);
-	}
-	if (check_args(argv))
+	if (((argc != 5) && (argc != 6)) || check_args(argv))
 	{
 		printf("Wrong arguments\n");
 		printf("Usage:\n");
 		printf("./philo number_of_philosophers time_to_die time_to_eat time_to_sleep [maximum_meals]\n");
 		return (0);
 	}
-  if (init(&data, argv))
-  {
-    printf("Initialization error\n");
-    return (1);
-  }
-  start_simulation(&data);
-  while (1)
-  {
-	usleep(250);
-	pthread_mutex_lock((&data.stop_mutex));
-	if (data.stop == 1)
+	if (init(&data, argv))
 	{
-		pthread_mutex_unlock((&data.stop_mutex));
-		end_simulation(&data);
-		return (0);
-	}
-	pthread_mutex_unlock((&data.stop_mutex));
-  }
-  end_simulation(&data);
-  free(data.philos);
-  free(data.forks);
+		printf("Initialization error\n");
+		return (1);
+ 	}
+ 	start_simulation(&data);
+	monitoring_loop(&data);
+  	end_simulation(&data);
+	free(data.philos);
+	free(data.forks);
 	return (0);
 }
