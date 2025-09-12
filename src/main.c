86c7cb218c922e4dcf9d78ae848670ad4e50caa7
dsemenov <dsemenov@student.42lyon.fr>
2025-09-12 23:33:47 +0200

    fix: improve monitoring loop by locking full_count_mutex to ensure thread safety

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 00:54:53 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/09/10 20:52:41 by dsemenov         ###   ########.fr       */
+/*   Updated: 2025/09/12 23:24:12 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,23 @@
 #include <unistd.h>
 #include "philo.h"
 #include <stdlib.h>
+#include <pthread.h>
 
 static int monitoring_loop(t_data *data)
 {
   while (1)
   {
 	usleep(250);
-	pthread_mutex_lock((&data->stop_mutex));
-	if (data->stop == 1)
+	pthread_mutex_lock(&data->stop_mutex);
+	pthread_mutex_lock(&data->full_count_mutex);
+	if (data->stop == 1 || data->full_count == data->num_of_philos)
 	{
 		pthread_mutex_unlock((&data->stop_mutex));
+		pthread_mutex_unlock(&data->full_count_mutex);
 		return (0);
 	}
 	pthread_mutex_unlock((&data->stop_mutex));
+	pthread_mutex_unlock(&data->full_count_mutex);
   }
 }
 
